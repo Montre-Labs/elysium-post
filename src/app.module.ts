fd3e93b0b77c46aefffe8a92b72bbdb579955516
fyoussef <fillipyuou@gmail.com>
2024-10-18 13:31:39 -0300

    chore: include message module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AdaptersModule } from './adapters/adapters.module';
 import { ConfigModule } from '@nestjs/config';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AdaptersModule],
+  imports: [ConfigModule.forRoot(), AdaptersModule, MessagesModule],
 })
 export class AppModule {}
