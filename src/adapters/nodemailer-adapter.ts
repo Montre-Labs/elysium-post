a35ba69e37505855946e66ad41c5abbb6d4d4edd
fyoussef <fillipyuou@gmail.com>
2024-10-18 13:30:42 -0300

    feat: add prisma adapter

--- src/adapters/nodemailer-adapter.ts
+++ src/adapters/nodemailer-adapter.ts
@@ -8,7 +8,7 @@ export class NodemailerAdapter {
     const transport = createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
